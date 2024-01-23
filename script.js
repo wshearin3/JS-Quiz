61eacf89ddce24b22a65f4e2614c38de45f9027d
Trey <trey.shearin3@gmail.com>
2024-01-22 19:06:20 -0500

    added TODO commenmts to JS file

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Quiz")
+
+// TODO Create main page with start button
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+// Mockup had timer start at 75 seconds
+
+// There is a link in the top left to view high scores
+// There is a timer in the top right counting down
+
+// When an answer is selected wrong or correct is presented along with the next question
