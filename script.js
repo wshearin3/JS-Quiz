f82a3d0162ca1bd9dc4854aeebb0519f42723db2
Trey <trey.shearin3@gmail.com>
2024-01-24 14:30:31 -0500

    deleted completed TODO comments

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 console.log("Quiz")
 
-// TODO Create main page with start button
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
@@ -13,10 +9,6 @@ console.log("Quiz")
 // WHEN the game is over
 // THEN I can save my initials and score
 
-// Mockup had timer start at 75 seconds
-
-// There is a link in the top left to view high scores
-// There is a timer in the top right counting down
 
 // When an answer is selected wrong or correct is presented along with the next question
 document.getElementById("main").style.display = "none";
